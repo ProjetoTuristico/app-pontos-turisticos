06362c47d5ff5da2b3420b805816abe7fd9dbcb9
ProjetoTuristico <projetoturisticograu@gmail.com>
2026-03-18 17:20:52 -0300

    Create turismo.sql

---
+++ database/turismo.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE turismo;
+USE turismo;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(255),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
