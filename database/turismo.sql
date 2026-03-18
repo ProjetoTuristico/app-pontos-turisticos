94118e1e28c078e22c8da0070c227526214133d1
ProjetoTuristico <projetoturisticograu@gmail.com>
2026-03-18 19:58:42 -0300

    Update turismo.sql

--- database/turismo.sql
+++ database/turismo.sql
@@ -9,9 +9,11 @@ CREATE TABLE usuarios (
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `pontos_turisticos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `descricao` text NOT NULL,
-  `endereco` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE pontos_turisticos (
+  id int(11) NOT NULL,
+  nome VARCHAR(150) DEFAULT NULL,
+  descricao text DEFAULT NULL,
+  cidade varchar(100) DEFAULT NULL,
+  estado varchar(50) DEFAULT NULL,
+  imagem_url text DEFAULT NULL,
+  categoria_id int(11) DEFAULT NULL
